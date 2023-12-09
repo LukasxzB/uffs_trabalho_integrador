189e7b418f38f3183e5170424cc8fe6eab7ff3c1
Lucas Belini <lucas.belini@inovadora.com.br>
2023-12-09 04:56:25 -0300

    Controller, service e module do produto criado

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,6 +4,7 @@ import { UserModule } from "./user/user.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ConfigModule } from "@nestjs/config";
 import { CustomerModule } from "./customer/customer.module";
+import { ProductModule } from "./product/product.module";
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { CustomerModule } from "./customer/customer.module";
     UserModule,
     PrismaModule,
     CustomerModule,
+    ProductModule,
   ],
 })
 export class AppModule {}
